62b3888f224c57be95c2d60345161d706b2e95f1
shanthini1002 <padmashanthini@gmail.com>
2025-02-26 11:11:01 +0530

    app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ if section == "Upload & Preview Data":
         st.dataframe(data)
 
 if 'data' in locals():
-    if section == "EDA & Visualization":
+    if section == "EDA ":
         st.title("Exploratory Data Analysis")
         st.write("### Data Info")
         st.write(data.info())
@@ -38,6 +38,7 @@ if 'data' in locals():
         
         st.write("### Checking for missing values")
         st.write(data.isnull().sum())
+        
      if section == " Visualization":
         # Pairplot visualization
         st.write("### Pairplot of Features")
