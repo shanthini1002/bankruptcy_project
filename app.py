4c563079bd783112ed67883001ee6d48bd7bc769
shanthini1002 <padmashanthini@gmail.com>
2025-02-27 09:37:54 +0530

    app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ if section == "EDA & Visualization":
 if section == "Model Building":
         st.title("Model Training")
         
-        X = data.drop(columns=['       class')
+        X = data.drop(columns=['       class'])
         y = data['       class']
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
         
