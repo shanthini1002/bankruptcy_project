5c3c05274f7ba592bd4b62f48872f6909fe650f5
shanthini1002 <padmashanthini@gmail.com>
2025-03-10 15:22:06 +0530

    app.py

--- app.py
+++ app.py
@@ -19,6 +19,14 @@ from sklearn.preprocessing import StandardScaler
 #     "Preview Data", "EDA & Visualization", "Model Building", "Model Evaluation", "Confusion Matrix", "Prediction App"
 # ])
 
+    def evaluate_model(model_name, y_true, y_pred):
+        st.write(f"### {model_name} Model Evaluation")
+        st.write(f"**Accuracy:** {accuracy_score(y_true, y_pred):.4f}")
+        st.write("**Classification Report:**")
+        st.text(classification_report(y_true, y_pred))
+        st.write("**Confusion Matrix:**")
+        st.text(confusion_matrix(y_true, y_pred))
+        
 st.sidebar.title("Navigation")
 section = st.sidebar.radio("Go to", [
     "Preview Data", "EDA & Visualization", "Model Building", "Model Evaluation", "Confusion Matrix", "Prediction App"
@@ -122,13 +130,7 @@ elif section == "Model Evaluation":
     st.title("Model Evaluation")
 
     # Define the function to evaluate the models
-    def evaluate_model(model_name, y_true, y_pred):
-        st.write(f"### {model_name} Model Evaluation")
-        st.write(f"**Accuracy:** {accuracy_score(y_true, y_pred):.4f}")
-        st.write("**Classification Report:**")
-        st.text(classification_report(y_true, y_pred))
-        st.write("**Confusion Matrix:**")
-        st.text(confusion_matrix(y_true, y_pred))
+
 
     
 
